ba2249d52d72ffe76c050e20a5aac377b83de12c
Luis <lvas248@gmail.com>
2022-05-16 14:09:58 -0400

    commit tree b114338c4e8a1868f9c6466f4c961fcbff35081e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+ const currentUser = 'Luis Vasquez';
+
+ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+ excitedWelcomeMessage;
+
+ const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
